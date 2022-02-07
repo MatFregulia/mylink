acf1a6d7a2707dfeb1e8505cd25b694b868280f4
Matheus Fregulia <matheus.gviva@gmail.com>
2022-02-07 09:13:52 -0300

    Criando a funcionalidade de encurtar o link

--- src/components/LinkItem/index.js
+++ src/components/LinkItem/index.js
@@ -1,7 +1,12 @@
 import './link-item.css'
 import { FiX, FiClipboard } from 'react-icons/fi'
 
-export default function LinkItem({ closeModal }) {
+export default function LinkItem({ closeModal, content }) {
+    async function copyLink() {
+        await navigator.clipboard.writeText(content.link)
+        alert("URL copiada com sucesso!")
+    }
+
     return(
         <div className="modal-container">
             
@@ -13,11 +18,11 @@ export default function LinkItem({ closeModal }) {
             </div>
 
             <span>
-                http://sujeitoprogramador.com
+                {content.long_url}
             </span>
 
-            <button className="modal-link">
-                http://bit.ly/12900
+            <button className="modal-link" onClick={copyLink}>
+                {content.link}
                 <FiClipboard size={20} color="#FFF" />
             </button>
 
--- src/services/api.js
+++ src/services/api.js
@@ -3,5 +3,10 @@ import axios from 'axios';
 export const key = 'fc242cc841cd565469940aaeef6879c5830aa77a'
 
 const api = axios.create({
-    baseURL: ''
+    baseURL: 'https://api-ssl.bitly.com/v4',
+    headers: {
+        'Authorization' : `Bearer ${key}`
+    }
 })
+
+export default api;
