c2c38eb84387df2012b2f152d870e9505b911f0b
Matheus Fregulia <matheus.gviva@gmail.com>
2022-02-07 10:23:57 -0300

    Criando uma lista de links

--- src/pages/Links/index.js
+++ src/pages/Links/index.js
@@ -1,8 +1,30 @@
+import { useState, useEffect } from 'react'
 import { FiArrowLeft, FiLink, FiTrash } from 'react-icons/fi'
 import './links.css'
 import { Link } from 'react-router-dom'
 
+import { getLinksSave } from '../../services/storeLinks'
+
 export default function Links() {
+    const [myLinks, setMyLinks] = useState([]);
+    const [data, setData] = useState({});
+    const [showModal, setShowModal] = useState(false);
+
+    useEffect(() => {
+      async function getLinks() {
+        const result = await getLinksSave('@encurtaLink');
+        
+        if (result.length === 0) {
+          console.log("Lista vazia")
+        }
+
+        setMyLinks(result);
+      }
+      
+      getLinks();
+
+    }, [])
+
     return(
       <div className='links/container'>
         
