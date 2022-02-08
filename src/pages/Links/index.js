a8ff79db55aaa5553dfc118c849b1e971331824c
Matheus Fregulia <matheus.gviva@gmail.com>
2022-02-08 12:16:12 -0300

    Termino do projeto

--- src/pages/Links/index.js
+++ src/pages/Links/index.js
@@ -3,19 +3,23 @@ import { FiArrowLeft, FiLink, FiTrash } from 'react-icons/fi'
 import './links.css'
 import { Link } from 'react-router-dom'
 
-import { getLinksSave } from '../../services/storeLinks'
+import { getLinksSave, deleteLink } from '../../services/storeLinks'
+import LinkItem from '../../components/LinkItem'
 
 export default function Links() {
     const [myLinks, setMyLinks] = useState([]);
+
     const [data, setData] = useState({});
     const [showModal, setShowModal] = useState(false);
+
+    const [emptyList, setEmptyList] = useState(false);
 
     useEffect(() => {
       async function getLinks() {
         const result = await getLinksSave('@encurtaLink');
         
         if (result.length === 0) {
-          console.log("Lista vazia")
+          setEmptyList(true);
         }
 
         setMyLinks(result);
@@ -24,6 +28,21 @@ export default function Links() {
       getLinks();
 
     }, [])
+
+    function handleOpenLink(link) {
+      setData(link);
+      setShowModal(true);
+    }
+
+    async function handleDelete(id) {
+      const result = await deleteLink(myLinks, id);
+
+      if (result.length === 0) {
+        setEmptyList(true);
+      }
+
+      setMyLinks(result);
+    }
 
     return(
       <div className='links/container'>
@@ -35,25 +54,30 @@ export default function Links() {
           <h1>Meus Links</h1>
         </div>
 
-        <div className="links-item">
-          <button className="link">
-            <FiLink size={18} color='#FFF' />
-              http://sujeitoprogramador.com
-          </button>
-          <buton className="link-delete">
-            <FiTrash size={24} color='#FF5454' />
-          </buton>
-        </div>
-        
-        <div className="links-item">
-          <button className="link">
-            <FiLink size={18} color='#FFF' />
-              http://sujeitoprogramador.com
-          </button>
-          <buton className="link-delete">
-            <FiTrash size={24} color='#FF5454' />
-          </buton>
-        </div>
+        { emptyList && (
+          <div className="links-item">
+            <h2 className="empty-text">Sua lista está vazia...</h2>
+          </div>
+        )}
+
+        { myLinks.map(link => (
+            <div key={Link.id} className="links-item">
+              <button className="link" onClick={ () => handleOpenLink(link) }>
+                <FiLink size={18} color='#FFF' />
+                  {link.long_url}
+              </button>
+              <button className="link-delete" onClick={ () => handleDelete(link.id) }>
+                <FiTrash size={24} color='#FF5454' />
+              </button>
+            </div>
+        ))}
+
+        { showModal && (
+          <LinkItem
+            closeModal={ () => setShowModal(false) }
+            content={data}
+          />
+        )}
 
       </div>
     )
--- src/services/storeLinks.js
+++ src/services/storeLinks.js
@@ -22,3 +22,14 @@ export async function saveLink(key, newLink) {
     console.log("Link salvo com sucesso!")
 }
 
+export function deleteLink(links, id) {
+    let myLinks = links.filter( item => {
+        return (item.id !== id)
+    })
+
+    localStorage.setItem('@encurtaLink', JSON.stringify(myLinks))
+    console.log("Link deletado com sucesso")
+
+    return myLinks
+}
+
